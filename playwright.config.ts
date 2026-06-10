bede0d2a1b66f5aa40e012efc68755bd364f97aa
TeohHW <teohhongwei@hotmail.com>
2026-06-10 11:50:52 +0800

    update config - final

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     }
-    /*,
+    ,
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
